4959756f14ec1fa4a820786ddf0c08577ff819eb
Anca Verniceanu <anca.verniceanu@gmail.com>
2021-01-04 13:21:22 +0100

    add node-sass and test css modules

--- components/Teaser.tsx
+++ components/Teaser.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
 import SbEditable from 'storyblok-react'
+import styles from './Teaser.module.scss';
 
 const Teaser = ({blok}) => {
   return (
     <SbEditable content={blok}>
-      <div className="bg-white-half">
-        <div className="pb-6 pt-16 container mx-auto">
-          <h2 className="text-6xl font-bold font-serif text-primary">{blok.headline}</h2>
-          <img src={blok.image && blok.image.filename} alt={blok.image && blok.image.alt} className="w-full" />
-        </div>
+      <div className={styles.container}>
+        <h2 className="text-6xl font-bold font-serif text-primary">{blok.headline}</h2>
+        <img src={blok.image && blok.image.filename} alt={blok.image && blok.image.alt} className={styles.image} />
       </div>
     </SbEditable>
   )
