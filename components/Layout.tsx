8cbf145492b8930298699e0db881a8cd462cd85d
Anca Verniceanu <anca.verniceanu@gmail.com>
2021-01-04 13:22:06 +0100

    cleanup project hardcoded content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,13 +4,13 @@ import Footer from './Footer'
 import StoryblokService from '../utils/storyblok-service'
 
 const Layout = ({ children, language }) => (
-  <div className="bg-gray-300">
+  <>
     <Head title="" description="" />
     <Navigation language={language} />
     {children}
     <Footer />
     {StoryblokService.bridge()}
-  </div>
+  </>
 )
 
 export default Layout
