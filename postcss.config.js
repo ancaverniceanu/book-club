9e7161d1c19fbdc7952c2243da07692a6cfeacda
Anca Verniceanu <anca.verniceanu@gmail.com>
2021-02-04 09:10:48 +0100

    Revert purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-    defaultExtractor: (content) => {
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    },
-  },
-];
 module.exports = {
-  plugins: ['tailwindcss', purgecss, 'postcss-preset-env'],
+  plugins: [
+    'tailwindcss',
+    [
+      'postcss-preset-env',
+      {
+        stage: 1,
+        features: {
+          'focus-within-pseudo-class': false,
+        },
+      },
+    ],
+  ],
 };
